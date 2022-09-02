4dcb12183ee32ce1f5dd156c7e5649b043bbf0ff
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-09-02 19:43:48 -0300

    Cria classe usuario

--- src/classes/Usuario.ts
+++ src/classes/Usuario.ts
@@ -1,1 +1,28 @@
-//classe Usuario
+export class Usuario {
+    private id:number | undefined;
+    nome:string;
+    email:string;
+    data_nasc:Date;
+    static  turma_id:number
+    constructor(nome:string, email:string, data_nasc:Date){
+        this.nome = nome;
+        this.email = email;
+        this.data_nasc = data_nasc;
+    }
+    public getIdUsuario(){
+        return this.id
+    }
+    public getNome():string {
+        return this.nome
+    }
+    public getEmail():string{
+        return this.email
+    }
+    public getDataNasc():Date{
+        return this.data_nasc
+    }
+    public setTurmaId(turma_id:number):void {
+        Usuario.turma_id = turma_id
+    }
+    
+}
