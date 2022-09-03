0bd270eb00c438bb6599b77463453a4787e2ab66
Bruno Pereira Assuncao <bruno.assuncao@grupoboticario.com.br>
2022-09-03 13:44:06 -0300

    Criando class da turma

--- src/classes/Turma.ts
+++ src/classes/Turma.ts
@@ -1,1 +1,15 @@
-// Classe Turma
+export class Turma {
+  private id: number | undefined;
+  nome: string;
+
+  constructor(nome: string) {
+    this.nome = nome;
+  }
+
+  public getIdTurma() {
+    return this.id;
+  }
+  public getNome(): string {
+    return this.nome;
+  }
+}
