b4d56e85d0895afa90587698aceb90e8cbd333b1
Maria Isabel Mendonca da Silva <mariaisabelmds1@gmail.com>
2022-09-06 11:00:15 -0300

    cria classe estudante

--- src/classes/Estudante.ts
+++ src/classes/Estudante.ts
@@ -1,1 +1,19 @@
-//Classe Estudante
+import { Usuario } from "./Usuario";
+
+export class Estudante extends Usuario {
+    hobbies: string[];
+
+    constructor(
+        nome: string,
+        email: string,
+        data_nasc: Date,
+        hobbies: string[]
+    ) {
+        super(nome, email, data_nasc)
+        this.hobbies = hobbies;
+    }
+
+    public getHobbies(): string[] {
+        return this.hobbies;
+    };
+}
