5a526c8bcbf3a877f83cc5b3a8fe944078641fb4
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-09-09 19:02:04 -0300

    Corrige função de mudar turma

--- src/endpoints/updateDocente.ts
+++ src/endpoints/updateDocente.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { DocenteDatabase } from "../service/DocenteDatabase";
 
 export const updateDocente = async (req:Request, res:Response) => {
-    const {id} = req.params;
+    const id = req.params.id;
     const especialidade_id = Number(req.body.especialidade_id); /*erro NaN quando o campo está preenchido */
     const turma_id = Number(req.body.turma_id)
 
@@ -32,13 +32,12 @@ export const updateDocente = async (req:Request, res:Response) => {
         if(turma_id){
             const userDatabase = new DocenteDatabase()
             const user = await userDatabase.getUserById(id)
-            if(user){
+            if(!user){
+                res.status(404).send("Id de usuário não localizado.")
+            }               
                 await userDatabase.setTurma(id, turma_id)
                 return res
                 .status(201).send("Turma do docente alterada com sucesso")
-            }else{
-                res.status(404).send("Id de usuário não localizado.")
-            }
         }
         return res.send("Fim da requisição")
     }catch(error:any) {
